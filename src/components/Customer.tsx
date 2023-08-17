c46d421fa6712c79a08b7357148c4a28ed10a854
unknown <izzulmaali3@gmail.com>
2023-08-17 15:38:14 +0700

    feat: added infinite scroll

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,17 +1,25 @@
 // @ts-nocheck
 import Image from 'next/image';
 import customers from '~/constan/customers';
+import Marquee from 'react-fast-marquee';
 
 export default function Customer() {
   return (
     <section className="my-20 lg:my-28 font-inter marquee">
-      <marquee scrollamount={5} width={'100%'} direction={'left'} loop={'INFINITE'}>
+      <Marquee pauseOnHover speed={40}>
         <div className="flex items-center gap-x-10 lg:gap-x-16">
           {customers.map((v) => (
-            <Image key={v.alt} src={v.link} alt={v.alt} width={70} height={70} className="lg:w-24 lg:h-fit" />
+            <Image
+              key={v.alt}
+              src={v.link}
+              alt={v.alt}
+              width={70}
+              height={70}
+              className="w-[70px] h-[70px] lg:w-24 lg:h-fit"
+            />
           ))}
         </div>
-      </marquee>
+      </Marquee>
     </section>
   );
 }
